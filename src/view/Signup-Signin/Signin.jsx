e8048ab1f4d61960570da2c81e641e5c837125da
moefawazz <hamoudirachido@gmail.com>
2023-12-05 17:56:45 +0200

    m

--- src/view/Signup-Signin/Signin.jsx
+++ src/view/Signup-Signin/Signin.jsx
@@ -14,7 +14,7 @@ const Signin = () => {
     setError('')
     try {
       await signIn(email, password)
-      navigate('/account')
+      navigate('/')
     } catch (e) {
       setError(e.message)
       console.log(e.message)
